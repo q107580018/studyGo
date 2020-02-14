61b1929ff6b80130902ab1cc86b56783c6d75298
q107580018 <107580018@qq.com>
2020-02-14 20:59:56 +0800

    update in 2020.02.14

---
+++ p5/05下载文件_服务端/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+// uploadFile 向客户端发送文件
+func uploadFile(conn net.Conn, addr, path string) {
+	defer conn.Close()
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Println("未找到客户需要的文件")
+		return
+	}
+	defer f.Close()
+	buf := make([]byte, 1024*1024)
+	// 读取内容
+	for {
+		n, err := f.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("文件发送完毕")
+			} else {
+				fmt.Println("f.Read err:", err)
+			}
+			return
+		}
+		// 发送内容
+		conn.Write(buf[:n])
+	}
+
+}
+func main() {
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("net.listen err:", err)
+		return
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("L.accept err:", err)
+			return
+		}
+		defer conn.Close()
+		addr := conn.RemoteAddr().String()
+		fmt.Println(addr, "已连接本服务器....")
+		buf := make([]byte, 512)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("conn.Read err:", err)
+			return
+		}
+		path := string(buf[:n])
+		go uploadFile(conn, addr, path)
+	}
+}
---
+++ p5/06下载文件_客户端/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"path/filepath"
+)
+
+func downloadFile(conn net.Conn, path string) {
+	_, fileName := filepath.Split(path)
+	f, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("os.Create err:", err)
+		return
+	}
+	defer f.Close()
+	_, err = conn.Write([]byte(path))
+	if err != nil {
+		fmt.Println("conn.Write err:", err)
+		return
+	}
+	buf := make([]byte, 1024*1024)
+	for {
+		// 读取下载文件
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("文件下载完毕")
+			} else {
+				fmt.Println("conn.Read err:", err)
+			}
+			return
+		}
+		// 写入文件
+		f.Write(buf[:n])
+	}
+}
+
+func main() {
+	list := os.Args
+	if len(list) != 3 {
+		fmt.Println("usage: xxx 127.0.0.1:8080 1.jpg")
+		return
+	}
+	ip := list[1]
+	path := list[2]
+	conn, err := net.Dial("tcp", ip)
+	if err != nil {
+		fmt.Println("net.Dial err:", err)
+		return
+	}
+	defer conn.Close()
+	downloadFile(conn, path)
+
+}
